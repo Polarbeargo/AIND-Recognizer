fb9551019a560183ecd80a1659143e9ec5a8f984
alice <bow1226@gmail.com>
2018-05-24 06:02:00 +0800

    Implement recognize test word sequences from word models set. Return: (list, list)  as probabilities, guesses.
    Implement recognize test word sequences from word models set. Return: (list, list)  as probabilities, guesses.

--- my_recognizer.py
+++ my_recognizer.py
@@ -16,11 +16,26 @@ def recognize(models: dict, test_set: SinglesData):
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    warnings.filterwarnings("ignore", category = DeprecationWarning)
     probabilities = []
     guesses = []
 
     # TODO implement the recognizer
     # return probabilities, guesses
-    
-    raise NotImplementedError
+    for item in range(test_set.num_items):
+
+        seq, length = test_set.get_item_Xlengths(item)
+        prob_rec, best_guess, highest_prob = {}, None, None
+
+        for word, model in models.items():
+            try:
+                prob_rec[word] = model.score(seq, length)
+                if highest_prob < prob_rec[word] or highest_prob is None:
+                   highest_prob, best_guess = prob_rec[word], word
+            except:
+                prob_rec[word] = None
+                
+        probabilities.append(prob_rec)
+        guesses.append(best_guess)
+
+    return probabilities, guesses
